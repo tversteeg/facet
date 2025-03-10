ae53e41a610e56a13a118c94fef6ce7702cf2a46
Amos Wenger <amos@bearcove.net>
2025-03-10 23:11:52 +0100

    And now the shapely-json test works!

--- shapely/tests/derive.rs
+++ shapely/tests/derive.rs
@@ -1,13 +1,13 @@
+use insta::assert_debug_snapshot;
 use shapely::Shapely;
 
 #[test]
 fn simple_struct() {
     #[derive(Debug, Shapely)]
-    #[allow(dead_code)]
     struct Blah {
         foo: u32,
         bar: String,
     }
 
-    eprintln!("{:?}", Blah::shape())
+    assert_debug_snapshot!(Blah::shape());
 }
