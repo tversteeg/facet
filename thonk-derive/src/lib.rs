2d168c8cfdd0042c9fbc898b7b9d96a4154b227d
Amos Wenger <amos@bearcove.net>
2025-03-10 13:14:20 +0100

    start on derive

---
+++ thonk-derive/src/lib.rs
@@ -0,0 +1,22 @@
+// use proc_macro::TokenStream;
+use unsynn::*;
+
+unsynn! {
+    enum Enum {
+        Two(Plus, Plus, Dot),
+        One(Plus, Dot),
+        TwoS { a: Minus, b: Minus, c: Dot},
+        OneS { a: Minus, b: Dot },
+        // the Expect<Dollar> shows a rust-analyzer error here, which is probably a bug in r-a
+        PunctBreak(Punct, Expect<Dollar>),
+    }
+}
+
+#[proc_macro_derive(Thonk)]
+pub fn thonk_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = TokenStream::from(input);
+    let mut i = input.to_token_iter();
+    let parsed: Enum = i.parse().unwrap();
+    let s = format!("parsed: {parsed:?}");
+    s.into_token_stream().into()
+}
