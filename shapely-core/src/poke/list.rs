c18b10d1115f87f1eba76008900211798ec2b439
Amos Wenger <amos@bearcove.eu>
2025-04-01 19:51:36 +0200

    more indirection for the indirection gods!

--- shapely-core/src/poke/list.rs
+++ shapely-core/src/poke/list.rs
@@ -1,4 +1,4 @@
-use crate::{ListDef, Opaque, Shape, ValueVTable};
+use crate::{ListDef, ListVTable, Opaque, Shape, ValueVTable};
 
 /// Allows poking a list (appending, etc.)
 pub struct PokeList<'mem> {
@@ -8,6 +8,7 @@ pub struct PokeList<'mem> {
     #[allow(dead_code)]
     vtable: ValueVTable,
     list_def: ListDef,
+    list_vtable: ListVTable,
 }
 
 impl<'mem> PokeList<'mem> {
@@ -22,11 +23,13 @@ impl<'mem> PokeList<'mem> {
         vtable: ValueVTable,
         list_def: ListDef,
     ) -> Self {
+        let list_vtable = (list_def.vtable)();
         Self {
             data,
             shape,
             vtable,
             list_def,
+            list_vtable,
         }
     }
 
@@ -37,12 +40,12 @@ impl<'mem> PokeList<'mem> {
     /// `item` is moved out of (with [`std::ptr::read`]) — it should be deallocated
     /// afterwards but NOT dropped.
     pub unsafe fn push<'src>(&mut self, item: Opaque<'src>) {
-        unsafe { (self.list_def.vtable.push)(self.data, item) }
+        unsafe { (self.list_vtable.push)(self.data, item) }
     }
 
     /// Gets the number of items in the list
     pub fn len(&self) -> usize {
-        unsafe { (self.list_def.vtable.len)(self.data.as_const()) }
+        unsafe { (self.list_vtable.len)(self.data.as_const()) }
     }
 
     /// Returns true if the list is empty
@@ -56,6 +59,6 @@ impl<'mem> PokeList<'mem> {
     ///
     /// Panics if the index is out of bounds.
     pub fn get_item_ptr(&self, index: usize) -> crate::OpaqueConst {
-        unsafe { (self.list_def.vtable.get_item_ptr)(self.data.as_const(), index) }
+        unsafe { (self.list_vtable.get_item_ptr)(self.data.as_const(), index) }
     }
 }
--- shapely-core/src/poke/map.rs
+++ shapely-core/src/poke/map.rs
@@ -1,4 +1,4 @@
-use crate::{MapDef, Opaque, OpaqueConst, Shape, ValueVTable};
+use crate::{MapDef, MapVTable, Opaque, OpaqueConst, Shape, ValueVTable};
 
 /// Allows poking a map (inserting, etc.)
 pub struct PokeMap<'mem> {
@@ -8,6 +8,7 @@ pub struct PokeMap<'mem> {
     #[allow(dead_code)]
     vtable: ValueVTable,
     map_def: MapDef,
+    map_vtable: MapVTable,
 }
 
 impl<'mem> PokeMap<'mem> {
@@ -22,11 +23,13 @@ impl<'mem> PokeMap<'mem> {
         vtable: ValueVTable,
         map_def: MapDef,
     ) -> Self {
+        let map_vtable = (map_def.vtable)();
         Self {
             data,
             shape,
             vtable,
             map_def,
+            map_vtable,
         }
     }
 
@@ -37,12 +40,12 @@ impl<'mem> PokeMap<'mem> {
     /// `key` and `value` are moved out of (with [`std::ptr::read`]) — they should be deallocated
     /// afterwards but NOT dropped.
     pub unsafe fn insert<'key, 'value>(&mut self, key: Opaque<'key>, value: Opaque<'value>) {
-        unsafe { (self.map_def.vtable.insert)(self.data, key, value) }
+        unsafe { (self.map_vtable.insert)(self.data, key, value) }
     }
 
     /// Gets the number of entries in the map
     pub fn len(&self) -> usize {
-        unsafe { (self.map_def.vtable.len)(self.data.as_const()) }
+        unsafe { (self.map_vtable.len)(self.data.as_const()) }
     }
 
     /// Checks if the map contains no entries
@@ -52,13 +55,13 @@ impl<'mem> PokeMap<'mem> {
 
     /// Checks if the map contains a key
     pub fn contains_key<'key>(&self, key: OpaqueConst<'key>) -> bool {
-        unsafe { (self.map_def.vtable.contains_key)(self.data.as_const(), key) }
+        unsafe { (self.map_vtable.contains_key)(self.data.as_const(), key) }
     }
 
     /// Gets a pointer to the value for a given key
     ///
     /// Returns `None` if the key is not found.
     pub fn get_value_ptr<'key>(&self, key: OpaqueConst<'key>) -> Option<OpaqueConst<'mem>> {
-        unsafe { (self.map_def.vtable.get_value_ptr)(self.data.as_const(), key) }
+        unsafe { (self.map_vtable.get_value_ptr)(self.data.as_const(), key) }
     }
 }
