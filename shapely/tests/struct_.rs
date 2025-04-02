e32c5154db26d1f97d9a03ceea1191d72e8fa5fc
Amos Wenger <amos@bearcove.eu>
2025-04-03 00:06:16 +0200

    new structure works

--- shapely/tests/struct_.rs
+++ shapely/tests/struct_.rs
@@ -1,4 +1,4 @@
-use shapely::{OpaqueConst, Poke, Shapely};
+use shapely::{OpaqueConst, OpaqueUninit, Poke, Shapely};
 
 // Allow dead code in test modules since we're not constructing all enum variants
 #[allow(dead_code)]
@@ -6,6 +6,15 @@ use shapely::{OpaqueConst, Poke, Shapely};
 struct FooBar {
     foo: u64,
     bar: String,
+}
+
+impl Default for FooBar {
+    fn default() -> Self {
+        FooBar {
+            foo: 69,
+            bar: String::new(),
+        }
+    }
 }
 
 #[test]
@@ -35,4 +44,53 @@ fn build_foobar_through_reflection() {
         },
         foo_bar
     )
+}
+
+#[test]
+#[should_panic(expected = "Field 'bar' was not initialized")]
+fn build_foobar_incomplete() {
+    let mut poke = Poke::alloc::<FooBar>().into_struct();
+    poke.set_by_name("foo", OpaqueConst::from_ref(&42u64))
+        .unwrap();
+
+    let foo_bar = poke.build::<FooBar>();
+
+    // Verify the fields were set correctly
+    assert_eq!(foo_bar.foo, 42);
+    assert_eq!(foo_bar.bar, "Hello, World!");
+
+    assert_eq!(
+        FooBar {
+            foo: 42,
+            bar: "Hello, World!".to_string()
+        },
+        foo_bar
+    )
+}
+
+#[test]
+fn build_foobar_after_default() {
+    let mut foo_bar: FooBar = Default::default();
+
+    let mut poke = unsafe {
+        Poke::from_opaque_uninit(OpaqueUninit::new(&mut foo_bar as *mut _), FooBar::SHAPE_FN)
+    }
+    .into_struct();
+    unsafe {
+        poke.mark_initialized(0);
+        poke.mark_initialized(1);
+    }
+
+    {
+        let bar = String::from("Hello, World!");
+        poke.set_by_name("bar", OpaqueConst::from_ref(&bar))
+            .unwrap();
+        // bar has been moved out of
+        std::mem::forget(bar);
+    }
+    poke.build_in_place();
+
+    // Verify the fields were set correctly
+    assert_eq!(foo_bar.foo, 69);
+    assert_eq!(foo_bar.bar, "Hello, World!");
 }
--- shapely/tests/value.rs
+++ shapely/tests/value.rs
@@ -12,21 +12,3 @@ fn build_u64_properly() {
     // Verify the value was set correctly
     assert_eq!(value, 42);
 }
-
-#[test]
-#[should_panic(expected = "Field '0' was not initialized")]
-fn build_u64_unfilled() {
-    use crate::{Poke, Shapely};
-
-    let shape = u64::shape();
-    eprintln!("{shape:#?}");
-
-    let poke = Poke::alloc::<u64>().into_scalar();
-
-    // Intentionally not filling the value
-    let _value = poke
-        .into_scalar()
-        .default_in_place()
-        .expect("Failed to set default");
-    // This should panic
-}
