2c0e13a3fcd622218d80778b5eee148f1804d967
Amos Wenger <amos@bearcove.eu>
2025-04-03 14:25:29 +0200

    Uncomment a bunch of tests

--- shapely/tests/derive.rs
+++ shapely/tests/derive.rs
@@ -1,177 +1,177 @@
-// use shapely::{Def, Shapely, StructDef};
-// use std::mem::offset_of;
+use shapely::{Def, Shapely, StructDef};
+use std::mem::offset_of;
 
-// #[test]
-// fn simple_struct() {
-//     #[derive(Debug, Shapely)]
-//     struct Blah {
-//         foo: u32,
-//         bar: String,
-//     }
+#[test]
+fn simple_struct() {
+    #[derive(Debug, Shapely)]
+    struct Blah {
+        foo: u32,
+        bar: String,
+    }
 
-//     if !cfg!(miri) {
-//         let shape = Blah::SHAPE;
+    if !cfg!(miri) {
+        let shape = Blah::SHAPE;
 
-//         // Check the name using Display
-//         assert_eq!(format!("{}", shape), "Blah");
+        // Check the name using Display
+        assert_eq!(format!("{}", shape), "Blah");
 
-//         assert_eq!(shape.layout.size(), 32);
-//         assert_eq!(shape.layout.align(), 8);
+        assert_eq!(shape.layout.size(), 32);
+        assert_eq!(shape.layout.align(), 8);
 
-//         if let Def::Struct(StructDef { fields }) = shape.def {
-//             assert_eq!(fields.len(), 2);
+        if let Def::Struct(StructDef { fields }) = shape.def {
+            assert_eq!(fields.len(), 2);
 
-//             let foo_field = &fields[0];
-//             assert_eq!(foo_field.name, "foo");
-//             assert_eq!(foo_field.shape.layout.size(), 4);
-//             assert_eq!(foo_field.shape.layout.align(), 4);
-//             assert_eq!(foo_field.offset, offset_of!(Blah, foo));
+            let foo_field = &fields[0];
+            assert_eq!(foo_field.name, "foo");
+            assert_eq!(foo_field.shape.layout.size(), 4);
+            assert_eq!(foo_field.shape.layout.align(), 4);
+            assert_eq!(foo_field.offset, offset_of!(Blah, foo));
 
-//             let bar_field = &fields[1];
-//             assert_eq!(bar_field.name, "bar");
-//             assert_eq!(bar_field.shape.layout.size(), 24);
-//             assert_eq!(bar_field.shape.layout.align(), 8);
-//             assert_eq!(bar_field.offset, offset_of!(Blah, bar));
-//         } else {
-//             panic!("Expected Struct innards");
-//         }
-//     }
-// }
+            let bar_field = &fields[1];
+            assert_eq!(bar_field.name, "bar");
+            assert_eq!(bar_field.shape.layout.size(), 24);
+            assert_eq!(bar_field.shape.layout.align(), 8);
+            assert_eq!(bar_field.offset, offset_of!(Blah, bar));
+        } else {
+            panic!("Expected Struct innards");
+        }
+    }
+}
 
-// #[test]
-// fn struct_repr_c() {
-//     #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
-//     #[repr(C)]
-//     struct Blah {
-//         foo: u32,
-//         bar: String,
-//     }
-// }
+#[test]
+fn struct_repr_c() {
+    #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
+    #[repr(C)]
+    struct Blah {
+        foo: u32,
+        bar: String,
+    }
+}
 
-// #[test]
-// fn struct_doc_comment() {
-//     #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
-//     /// yes
-//     struct Foo {}
-// }
+#[test]
+fn struct_doc_comment() {
+    #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
+    /// yes
+    struct Foo {}
+}
 
-// #[test]
-// fn struct_field_doc_comment() {
-//     #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
-//     struct Foo {
-//         /// This field has a doc comment
-//         bar: u32,
-//     }
-// }
+#[test]
+fn struct_field_doc_comment() {
+    #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
+    struct Foo {
+        /// This field has a doc comment
+        bar: u32,
+    }
+}
 
-// #[test]
-// fn struct_with_pub_field() {
-//     #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
-//     struct Foo {
-//         /// This is a public field
-//         pub bar: u32,
-//     }
-// }
+#[test]
+fn struct_with_pub_field() {
+    #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
+    struct Foo {
+        /// This is a public field
+        pub bar: u32,
+    }
+}
 
-// #[test]
-// fn tuple_struct_repr_transparent() {
-//     #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
-//     #[repr(transparent)]
-//     struct Blah(u32);
-// }
+#[test]
+fn tuple_struct_repr_transparent() {
+    #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
+    #[repr(transparent)]
+    struct Blah(u32);
+}
 
-// #[test]
-// fn tuple_struct_doc_comment() {
-//     #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
-//     #[repr(transparent)]
-//     /// This is a struct for sure
-//     struct Blah(u32);
-// }
+#[test]
+fn tuple_struct_doc_comment() {
+    #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
+    #[repr(transparent)]
+    /// This is a struct for sure
+    struct Blah(u32);
+}
 
-// #[test]
-// fn tuple_struct_field_doc_comment() {
-//     #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
-//     #[repr(transparent)]
-//     /// This is a struct for sure
-//     struct Blah(
-//         /// and this is a field
-//         u32,
-//     );
-// }
+#[test]
+fn tuple_struct_field_doc_comment() {
+    #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
+    #[repr(transparent)]
+    /// This is a struct for sure
+    struct Blah(
+        /// and this is a field
+        u32,
+    );
+}
 
-// #[test]
-// fn tuple_struct_with_pub_field() {
-//     #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
-//     #[repr(transparent)]
-//     /// This is a struct for sure
-//     struct Blah(
-//         /// and this is a field
-//         pub u32,
-//     );
-// }
+#[test]
+fn tuple_struct_with_pub_field() {
+    #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
+    #[repr(transparent)]
+    /// This is a struct for sure
+    struct Blah(
+        /// and this is a field
+        pub u32,
+    );
+}
 
-// #[test]
-// fn cfg_attrs() {
-//     #[derive(Shapely)]
-//     #[cfg_attr(feature = "testfeat", derive(Serialize, Deserialize))]
-//     #[cfg_attr(feature = "testfeat", serde(deny_unknown_fields))]
-//     pub struct CubConfig {}
-// }
+#[test]
+fn cfg_attrs() {
+    #[derive(Shapely)]
+    #[cfg_attr(feature = "testfeat", derive(Serialize, Deserialize))]
+    #[cfg_attr(feature = "testfeat", serde(deny_unknown_fields))]
+    pub struct CubConfig {}
+}
 
-// #[test]
-// fn cfg_attrs2() {
-//     #[derive(Shapely)]
-//     #[cfg_attr(feature = "testfeat", derive(Serialize, Deserialize))]
-//     #[cfg_attr(feature = "testfeat", serde(deny_unknown_fields))]
-//     pub struct CubConfig {
-//         /// size the disk cache is allowed to use
-//         #[cfg_attr(feature = "testfeat", serde(skip_serializing))]
-//         #[cfg_attr(
-//             feature = "testfeat",
-//             serde(default = "serde_defaults::default_disk_cache_size")
-//         )]
-//         pub disk_cache_size: String,
-//     }
-// }
+#[test]
+fn cfg_attrs2() {
+    #[derive(Shapely)]
+    #[cfg_attr(feature = "testfeat", derive(Serialize, Deserialize))]
+    #[cfg_attr(feature = "testfeat", serde(deny_unknown_fields))]
+    pub struct CubConfig {
+        /// size the disk cache is allowed to use
+        #[cfg_attr(feature = "testfeat", serde(skip_serializing))]
+        #[cfg_attr(
+            feature = "testfeat",
+            serde(default = "serde_defaults::default_disk_cache_size")
+        )]
+        pub disk_cache_size: String,
+    }
+}
 
-// #[test]
-// fn struct_with_std_string() {
-//     #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
-//     struct FileInfo {
-//         path: std::string::String,
-//         size: u64,
-//     }
-// }
+#[test]
+fn struct_with_std_string() {
+    #[derive(Clone, Hash, PartialEq, Eq, ::shapely::Shapely)]
+    struct FileInfo {
+        path: std::string::String,
+        size: u64,
+    }
+}
 
-// #[test]
-// fn derive_real_life_cub_config() {
-//     #[derive(Shapely)]
-//     #[cfg_attr(feature = "testfeat", derive(Serialize, Deserialize))]
-//     #[cfg_attr(feature = "testfeat", serde(deny_unknown_fields))]
-//     pub struct CubConfig {
-//         /// size the disk cache is allowed to use
-//         #[cfg_attr(feature = "testfeat", serde(skip_serializing))]
-//         #[cfg_attr(
-//             feature = "testfeat",
-//             serde(default = "serde_defaults::default_disk_cache_size")
-//         )]
-//         pub disk_cache_size: String,
+#[test]
+fn derive_real_life_cub_config() {
+    #[derive(Shapely)]
+    #[cfg_attr(feature = "testfeat", derive(Serialize, Deserialize))]
+    #[cfg_attr(feature = "testfeat", serde(deny_unknown_fields))]
+    pub struct CubConfig {
+        /// size the disk cache is allowed to use
+        #[cfg_attr(feature = "testfeat", serde(skip_serializing))]
+        #[cfg_attr(
+            feature = "testfeat",
+            serde(default = "serde_defaults::default_disk_cache_size")
+        )]
+        pub disk_cache_size: String,
 
-//         /// Listen address without http, something like "127.0.0.1:1111"
-//         #[cfg_attr(feature = "testfeat", serde(default = "serde_defaults::address"))]
-//         pub address: std::string::String,
+        /// Listen address without http, something like "127.0.0.1:1111"
+        #[cfg_attr(feature = "testfeat", serde(default = "serde_defaults::address"))]
+        pub address: std::string::String,
 
-//         /// Something like `http://localhost:1118`
-//         /// or `http://mom.svc.cluster.local:1118`, never
-//         /// a trailing slash.
-//         #[cfg_attr(feature = "testfeat", serde(default = "serde_defaults::mom_base_url"))]
-//         pub mom_base_url: String,
+        /// Something like `http://localhost:1118`
+        /// or `http://mom.svc.cluster.local:1118`, never
+        /// a trailing slash.
+        #[cfg_attr(feature = "testfeat", serde(default = "serde_defaults::mom_base_url"))]
+        pub mom_base_url: String,
 
-//         /// API key used to talk to mom
-//         #[cfg_attr(feature = "testfeat", serde(default = "serde_defaults::mom_api_key"))]
-//         pub mom_api_key: String,
-//     }
-// }
+        /// API key used to talk to mom
+        #[cfg_attr(feature = "testfeat", serde(default = "serde_defaults::mom_api_key"))]
+        pub mom_api_key: String,
+    }
+}
 
 // #[test]
 // fn struct_with_tuple() {
