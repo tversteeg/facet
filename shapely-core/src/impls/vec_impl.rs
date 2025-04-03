5c5c9db747cabef2478af2e4502e0509833c7d83
Amos Wenger <amos@bearcove.eu>
2025-04-03 12:54:46 +0200

    mhmh

--- shapely-core/src/impls/vec_impl.rs
+++ shapely-core/src/impls/vec_impl.rs
@@ -5,36 +5,36 @@ use std::{
 
 use crate::{Def, ListDef, ListVTable, OpaqueConst, Shape, Shapely, ValueVTable};
 
+/// proxies, using the <https://docs.rs/spez> trick, kind of:
+pub struct Wrap<T>(T);
+
+pub trait ViaDebug {
+    fn debug_fn(&self, f: &mut fmt::Formatter<'_>) -> Option<Result<(), fmt::Error>>;
+}
+
+pub trait ViaNone {
+    fn debug_fn(&self, f: &mut fmt::Formatter<'_>) -> Option<Result<(), fmt::Error>>;
+}
+
+impl<T: fmt::Debug> ViaDebug for &Wrap<T> {
+    fn debug_fn(&self, f: &mut fmt::Formatter<'_>) -> Option<Result<(), fmt::Error>> {
+        eprintln!("yes Debug");
+        Some(fmt::Debug::fmt(&self.0, f))
+    }
+}
+
+impl<T> ViaNone for Wrap<T> {
+    fn debug_fn(&self, _f: &mut fmt::Formatter<'_>) -> Option<Result<(), fmt::Error>> {
+        eprintln!("no Debug");
+        None
+    }
+}
+
 impl<T> Shapely for Vec<T>
 where
-    T: Shapely,
+    T: Shapely + fmt::Debug,
 {
     const SHAPE: &'static Shape = &const {
-        /// proxies, using the <https://docs.rs/spez> trick, kind of:
-        pub struct Wrap<T>(T);
-
-        pub trait ViaDebug {
-            fn debug_fn(&self, f: &mut fmt::Formatter<'_>) -> Option<Result<(), fmt::Error>>;
-        }
-
-        pub trait ViaNone {
-            fn debug_fn(&self, f: &mut fmt::Formatter<'_>) -> Option<Result<(), fmt::Error>>;
-        }
-
-        impl<T: fmt::Debug> ViaDebug for &Wrap<T> {
-            fn debug_fn(&self, f: &mut fmt::Formatter<'_>) -> Option<Result<(), fmt::Error>> {
-                eprintln!("yes Debug");
-                Some(fmt::Debug::fmt(&self.0, f))
-            }
-        }
-
-        impl<T> ViaNone for Wrap<T> {
-            fn debug_fn(&self, _f: &mut fmt::Formatter<'_>) -> Option<Result<(), fmt::Error>> {
-                eprintln!("no Debug");
-                None
-            }
-        }
-
         Shape {
             layout: Layout::new::<Vec<T>>(),
             vtable: &ValueVTable {
@@ -50,8 +50,9 @@ where
                 // TODO: specialize these
                 display: None,
                 debug: |value, f| {
-                    let val = Wrap(value);
-                    val.debug_fn(f)
+                    let v: &Vec<T> = unsafe { value.as_ref() };
+                    let val = Wrap(v);
+                    (&&val).debug_fn(f)
                 },
                 // TODO: specialize these
                 eq: None,
