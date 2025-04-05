33ad1a4be5122fbc772093b847c811e4d2347a3f
Amos Wenger <amos@bearcove.eu>
2025-04-05 16:35:16 +0200

    mh

--- shapely-samplelibc/src/example.c
+++ shapely-samplelibc/src/example.c
@@ -1,4 +1,6 @@
 
+#include <stdint.h>
+
 #if defined(_WIN32) || defined(_WIN64)
     #define EXPORT __declspec(dllexport)
 #elif defined(__GNUC__)
@@ -7,6 +9,30 @@
     #define EXPORT
 #endif
 
+typedef struct Bar {
+    int32_t a;
+    int32_t b;
+} Bar;
+
+typedef struct Foo {
+    int64_t x;
+    Bar bar;
+    uint32_t y;
+} Foo;
+
 EXPORT const char* get_library_message(void) {
     return "IAMA C lib AMA";
+}
+
+EXPORT Foo* get_foo(void) {
+    static Foo sample_foo = {
+        .x = 42,
+        .bar = {
+            .a = 10,
+            .b = 20
+        },
+        .y = 30
+    };
+
+    return &sample_foo;
 }
--- shapely-samplelibc/src/lib.rs
+++ shapely-samplelibc/src/lib.rs
@@ -1,5 +1,23 @@
+use shapely::Shapely;
+
 unsafe extern "C" {
     pub unsafe fn get_library_message() -> *const std::ffi::c_char;
+    pub unsafe fn get_foo() -> *const Foo;
+}
+
+#[derive(Shapely)]
+#[repr(C)]
+pub struct Foo {
+    pub x: i64,
+    pub bar: Bar,
+    pub y: i64,
+}
+
+#[derive(Shapely)]
+#[repr(C)]
+pub struct Bar {
+    pub a: i32,
+    pub b: i32,
 }
 
 #[cfg(test)]
@@ -7,12 +25,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn message() {
         let msg = unsafe { get_library_message() };
         let c_str = unsafe { std::ffi::CStr::from_ptr(msg) };
         let rust_str = c_str.to_string_lossy();
         println!("{}", rust_str);
 
         assert_eq!(rust_str, "IAMA C lib AMA")
+    }
+
+    #[test]
+    fn foo() {
+        let foo = unsafe { get_foo() };
+        let bar = unsafe { &(*foo).bar };
+        let x = unsafe { (*foo).x };
+        let y = unsafe { (*foo).y };
+
+        println!("Foo: x={}, bar.a={}, bar.b={}, y={}", x, bar.a, bar.b, y);
     }
 }
