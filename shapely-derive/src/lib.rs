077e0bcb1668eb33c9c754162810a58ffed5adf7
Amos Wenger <amos@bearcove.eu>
2025-04-05 14:41:10 +0200

    errors gone

--- shapely-derive/src/lib.rs
+++ shapely-derive/src/lib.rs
@@ -269,72 +269,10 @@ impl shapely::Shapely for {struct_name} {{
     const SHAPE: &'static shapely::Shape = &const {{
         shapely::Shape {{
             layout: std::alloc::Layout::new::<Self>(),
-            vtable: &shapely::ValueVTable {{
-                type_name: |f, _opts| std::fmt::Write::write_str(f, "{struct_name}"),
-                display: if shapely::impls!(Self: std::fmt::Display) {{
-                    Some(|data, f| {{
-                        use shapely::spez::*;
-                        (&&Spez(unsafe {{ data.as_ref::<Self>() }})).spez_display(f)
-                    }})
-                }} else {{
-                    None
-                }},
-                debug: if shapely::impls!(Self: std::fmt::Debug) {{
-                    Some(|data, f| {{
-                        use shapely::spez::*;
-                        (&&Spez(unsafe {{ data.as_ref::<Self>() }})).spez_debug(f)
-                    }})
-                }} else {{
-                    None
-                }},
-                default_in_place: if shapely::impls!(Self: std::default::Default) {{
-                    Some(|target| {{
-                        use shapely::spez::*;
-                        Some((&&Spez(<Self as shapely::Shapely>::DUMMY)).spez_default_in_place(target))
-                    }})
-                }} else {{
-                    None
-                }},
-                clone_into: if shapely::impls!(Self: std::clone::Clone) {{
-                    Some(|src, dst| {{
-                        use shapely::spez::*;
-                        Some((&&Spez(unsafe {{ src.as_ref::<Self>() }})).spez_clone_into(dst))
-                    }})
-                }} else {{
-                    None
-                }},
-                eq: if shapely::impls!(Self: std::cmp::PartialEq) {{
-                    Some(|left, right| {{
-                        use shapely::spez::*;
-                        (&&Spez(unsafe {{ left.as_ref::<Self>() }}))
-                            .spez_eq(&&Spez(unsafe {{ right.as_ref::<Self>() }}))
-                    }})
-                }} else {{
-                    None
-                }},
-                ord: if shapely::impls!(Self: std::cmp::Ord) {{
-                    Some(|left, right| {{
-                        use shapely::spez::*;
-                        (&&Spez(unsafe {{ left.as_ref::<Self>() }}))
-                            .spez_cmp(&&Spez(unsafe {{ right.as_ref::<Self>() }}))
-                    }})
-                }} else {{
-                    None
-                }},
-                hash: if shapely::impls!(Self: std::hash::Hash) {{
-                    Some(|value, hasher_this, hasher_write_fn| {{
-                        use shapely::spez::*;
-                        use shapely::vtable::HasherProxy;
-                        (&&Spez(unsafe {{ value.as_ref::<Self>() }}))
-                            .spez_hash(&mut unsafe {{ HasherProxy::new(hasher_this, hasher_write_fn) }})
-                    }})
-                }} else {{
-                    None
-                }},
-                drop_in_place: Some(|data| unsafe {{ data.drop_in_place::<Self>() }}),
-                parse: None,
-                try_from: None,
-            }},
+            vtable: shapely::value_vtable!(
+                {struct_name},
+                |f, _opts| std::fmt::Write::write_str(f, "{struct_name}")
+            ),
             def: shapely::Def::Struct(shapely::StructDef {{
                 fields: shapely::struct_fields!({struct_name}, ({fields})),
             }}),
