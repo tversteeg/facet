6bb81455956020a3a9a1991a3a397a95c8af8417
Amos Wenger <amos@bearcove.eu>
2025-04-06 16:22:35 +0200

    formatting

--- facet-msgpack/src/to_msgpack.rs
+++ facet-msgpack/src/to_msgpack.rs
@@ -23,6 +23,27 @@ pub fn to_writer<W: Write>(peek: Peek<'_>, writer: &mut W) -> io::Result<()> {
                 } else if pv.shape().is_type::<u64>() {
                     let value = unsafe { pv.data().as_ref::<u64>() };
                     write_u64(writer, *value)
+                } else if pv.shape().is_type::<u32>() {
+                    let value = unsafe { pv.data().as_ref::<u32>() };
+                    write_u32(writer, *value)
+                } else if pv.shape().is_type::<u16>() {
+                    let value = unsafe { pv.data().as_ref::<u16>() };
+                    write_u16(writer, *value)
+                } else if pv.shape().is_type::<u8>() {
+                    let value = unsafe { pv.data().as_ref::<u8>() };
+                    write_u8(writer, *value)
+                } else if pv.shape().is_type::<i64>() {
+                    let value = unsafe { pv.data().as_ref::<i64>() };
+                    write_i64(writer, *value)
+                } else if pv.shape().is_type::<i32>() {
+                    let value = unsafe { pv.data().as_ref::<i32>() };
+                    write_i32(writer, *value)
+                } else if pv.shape().is_type::<i16>() {
+                    let value = unsafe { pv.data().as_ref::<i16>() };
+                    write_i16(writer, *value)
+                } else if pv.shape().is_type::<i8>() {
+                    let value = unsafe { pv.data().as_ref::<i8>() };
+                    write_i8(writer, *value)
                 } else {
                     todo!("Unsupported scalar type: {}", pv.shape())
                 }
@@ -77,6 +98,60 @@ fn write_str<W: Write>(writer: &mut W, s: &str) -> io::Result<()> {
     writer.write_all(bytes)
 }
 
+fn write_u8<W: Write>(writer: &mut W, n: u8) -> io::Result<()> {
+    match n {
+        0..=127 => {
+            // positive fixint
+            writer.write_all(&[n])
+        }
+        _ => {
+            // uint8
+            writer.write_all(&[0xcc, n])
+        }
+    }
+}
+
+fn write_u16<W: Write>(writer: &mut W, n: u16) -> io::Result<()> {
+    match n {
+        0..=127 => {
+            // positive fixint
+            writer.write_all(&[n as u8])
+        }
+        128..=255 => {
+            // uint8
+            writer.write_all(&[0xcc, n as u8])
+        }
+        _ => {
+            // uint16
+            writer.write_all(&[0xcd])?;
+            writer.write_all(&n.to_be_bytes())
+        }
+    }
+}
+
+fn write_u32<W: Write>(writer: &mut W, n: u32) -> io::Result<()> {
+    match n {
+        0..=127 => {
+            // positive fixint
+            writer.write_all(&[n as u8])
+        }
+        128..=255 => {
+            // uint8
+            writer.write_all(&[0xcc, n as u8])
+        }
+        256..=65535 => {
+            // uint16
+            writer.write_all(&[0xcd])?;
+            writer.write_all(&(n as u16).to_be_bytes())
+        }
+        _ => {
+            // uint32
+            writer.write_all(&[0xce])?;
+            writer.write_all(&n.to_be_bytes())
+        }
+    }
+}
+
 fn write_u64<W: Write>(writer: &mut W, n: u64) -> io::Result<()> {
     match n {
         0..=127 => {
@@ -101,6 +176,146 @@ fn write_u64<W: Write>(writer: &mut W, n: u64) -> io::Result<()> {
             // uint64
             writer.write_all(&[0xcf])?;
             writer.write_all(&n.to_be_bytes())
+        }
+    }
+}
+
+fn write_i8<W: Write>(writer: &mut W, n: i8) -> io::Result<()> {
+    match n {
+        -32..=-1 => {
+            // negative fixint
+            writer.write_all(&[n as u8])
+        }
+        -128..=-33 => {
+            // int8
+            writer.write_all(&[0xd0, n as u8])
+        }
+        0..=127 => {
+            // positive fixint
+            writer.write_all(&[n as u8])
+        }
+    }
+}
+
+fn write_i16<W: Write>(writer: &mut W, n: i16) -> io::Result<()> {
+    match n {
+        -32..=-1 => {
+            // negative fixint
+            writer.write_all(&[n as u8])
+        }
+        -128..=-33 => {
+            // int8
+            writer.write_all(&[0xd0, n as u8])
+        }
+        -32768..=-129 => {
+            // int16
+            writer.write_all(&[0xd1])?;
+            writer.write_all(&n.to_be_bytes())
+        }
+        0..=127 => {
+            // positive fixint
+            writer.write_all(&[n as u8])
+        }
+        128..=255 => {
+            // uint8
+            writer.write_all(&[0xcc, n as u8])
+        }
+        256..=32767 => {
+            // uint16
+            writer.write_all(&[0xcd])?;
+            writer.write_all(&(n as u16).to_be_bytes())
+        }
+    }
+}
+
+fn write_i32<W: Write>(writer: &mut W, n: i32) -> io::Result<()> {
+    match n {
+        -32..=-1 => {
+            // negative fixint
+            writer.write_all(&[n as u8])
+        }
+        -128..=-33 => {
+            // int8
+            writer.write_all(&[0xd0, n as u8])
+        }
+        -32768..=-129 => {
+            // int16
+            writer.write_all(&[0xd1])?;
+            writer.write_all(&(n as i16).to_be_bytes())
+        }
+        -2147483648..=-32769 => {
+            // int32
+            writer.write_all(&[0xd2])?;
+            writer.write_all(&n.to_be_bytes())
+        }
+        0..=127 => {
+            // positive fixint
+            writer.write_all(&[n as u8])
+        }
+        128..=255 => {
+            // uint8
+            writer.write_all(&[0xcc, n as u8])
+        }
+        256..=65535 => {
+            // uint16
+            writer.write_all(&[0xcd])?;
+            writer.write_all(&(n as u16).to_be_bytes())
+        }
+        65536..=2147483647 => {
+            // uint32
+            writer.write_all(&[0xce])?;
+            writer.write_all(&(n as u32).to_be_bytes())
+        }
+    }
+}
+
+fn write_i64<W: Write>(writer: &mut W, n: i64) -> io::Result<()> {
+    match n {
+        -32..=-1 => {
+            // negative fixint
+            writer.write_all(&[n as u8])
+        }
+        -128..=-33 => {
+            // int8
+            writer.write_all(&[0xd0, n as u8])
+        }
+        -32768..=-129 => {
+            // int16
+            writer.write_all(&[0xd1])?;
+            writer.write_all(&(n as i16).to_be_bytes())
+        }
+        -2147483648..=-32769 => {
+            // int32
+            writer.write_all(&[0xd2])?;
+            writer.write_all(&(n as i32).to_be_bytes())
+        }
+        i64::MIN..=-2147483649 => {
+            // int64
+            writer.write_all(&[0xd3])?;
+            writer.write_all(&n.to_be_bytes())
+        }
+        0..=127 => {
+            // positive fixint
+            writer.write_all(&[n as u8])
+        }
+        128..=255 => {
+            // uint8
+            writer.write_all(&[0xcc, n as u8])
+        }
+        256..=65535 => {
+            // uint16
+            writer.write_all(&[0xcd])?;
+            writer.write_all(&(n as u16).to_be_bytes())
+        }
+        65536..=4294967295 => {
+            // uint32
+            writer.write_all(&[0xce])?;
+            writer.write_all(&(n as u32).to_be_bytes())
+        }
+        4294967296..=i64::MAX => {
+            // uint64
+            writer.write_all(&[0xcf])?;
+            writer.write_all(&(n as u64).to_be_bytes())
         }
     }
 }
--- facet-msgpack/tests/serialize.rs
+++ facet-msgpack/tests/serialize.rs
@@ -12,7 +12,94 @@ use facet_trait::Facet;
 use facet_trait as facet;
 
 #[test]
-fn test_serialize() {
+fn test_integers() {
+    #[derive(Debug, PartialEq, Clone, Facet)]
+    struct IntegerTest {
+        pos_fixint: u8, // 0-127
+        uint8: u8,      // 128-255
+        uint16: u16,    // 256-65535
+        uint32: u32,    // up to 4294967295
+        uint64: u64,    // larger values
+        neg_fixint: i8, // -32 to -1
+        int8: i8,       // -128 to -33
+        int16: i16,     // -32768 to 32767
+        int32: i32,     // -2147483648 to 2147483647
+        int64: i64,     // -9223372036854775808 to 9223372036854775807
+    }
+
+    let test = IntegerTest {
+        pos_fixint: 127,
+        uint8: 255,
+        uint16: 65535,
+        uint32: 4294967295,
+        uint64: 18446744073709551615,
+        neg_fixint: -1,
+        int8: -128,
+        int16: -32768,
+        int32: -2147483648,
+        int64: -9223372036854775808,
+    };
+
+    let msgpack = to_vec(&test);
+
+    // Expected format:
+    // 8a                -- map with 10 elements
+    // aa706f735f666978696e74  -- "pos_fixint"
+    // 7f                -- 127 (positive fixint)
+    // a675696e7438     -- "uint8"
+    // cc ff            -- 255 (uint8)
+    // a775696e743136   -- "uint16"
+    // cd ff ff         -- 65535 (uint16)
+    // a775696e743332   -- "uint32"
+    // ce ff ff ff ff   -- 4294967295 (uint32)
+    // a775696e743634   -- "uint64"
+    // cf ff ff ff ff ff ff ff ff  -- 18446744073709551615 (uint64)
+    // aa6e65675f666978696e74  -- "neg_fixint"
+    // ff               -- -1 (negative fixint)
+    // a4696e7438       -- "int8"
+    // d0 80            -- -128 (int8)
+    // a5696e743136     -- "int16"
+    // d1 80 00         -- -32768 (int16)
+    // a5696e743332     -- "int32"
+    // d2 80 00 00 00   -- -2147483648 (int32)
+    // a5696e743634     -- "int64"
+    // d3 80 00 00 00 00 00 00 00  -- -9223372036854775808 (int64)
+
+    let expected = [
+        0x8a, // map with 10 elements
+        // pos_fixint
+        0xaa, 0x70, 0x6f, 0x73, 0x5f, 0x66, 0x69, 0x78, 0x69, 0x6e, 0x74, 0x7f, // 127
+        // uint8
+        0xa5, 0x75, 0x69, 0x6e, 0x74, 0x38, 0xcc, 0xff, // 255
+        // uint16
+        0xa6, 0x75, 0x69, 0x6e, 0x74, 0x31, 0x36, 0xcd, 0xff, 0xff, // 65535
+        // uint32
+        0xa6, 0x75, 0x69, 0x6e, 0x74, 0x33, 0x32, 0xce, 0xff, 0xff, 0xff, 0xff, // 4294967295
+        // uint64
+        0xa6, 0x75, 0x69, 0x6e, 0x74, 0x36, 0x34, 0xcf, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, // 18446744073709551615
+        // neg_fixint
+        0xaa, 0x6e, 0x65, 0x67, 0x5f, 0x66, 0x69, 0x78, 0x69, 0x6e, 0x74, 0xff, // -1
+        // int8
+        0xa4, 0x69, 0x6e, 0x74, 0x38, 0xd0, 0x80, // -128
+        // int16
+        0xa5, 0x69, 0x6e, 0x74, 0x31, 0x36, 0xd1, 0x80, 0x00, // -32768
+        // int32
+        0xa5, 0x69, 0x6e, 0x74, 0x33, 0x32, 0xd2, 0x80, 0x00, 0x00, 0x00, // -2147483648
+        // int64
+        0xa5, 0x69, 0x6e, 0x74, 0x36, 0x34, 0xd3, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, // -9223372036854775808
+    ];
+
+    assert_eq!(
+        msgpack, expected,
+        "\nGot:     {:02x?}\nExpected: {:02x?}",
+        msgpack, expected
+    );
+}
+
+#[test]
+fn test_struct() {
     #[derive(Debug, PartialEq, Clone, Facet)]
     struct TestStruct {
         name: String,
@@ -31,10 +118,10 @@ fn test_serialize() {
     // a4 6e616d65      -- "name" (length 4)
     // a5 416c696365    -- "Alice" (length 5)
     // a3 616765        -- "age" (length 3)
-    // ce 0000001e      -- 30 as uint32
+    // 1e               -- 30 as positive fixint
     let expected = [
         0x82, 0xa4, 0x6e, 0x61, 0x6d, 0x65, 0xa5, 0x41, 0x6c, 0x69, 0x63, 0x65, 0xa3, 0x61, 0x67,
-        0x65, 0xce, 0x00, 0x00, 0x00, 0x1e,
+        0x65, 0x1e,
     ];
 
     assert_eq!(msgpack, expected);
