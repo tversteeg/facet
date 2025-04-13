db8e8f44a216e12a973f3b23f18d4a7a02efffb6
Thomas Versteeg <t@versteeg.email>
2025-04-13 11:23:23 +0700

    fix(toml): implement a big chunk of enum parsing

--- facet-toml/src/lib.rs
+++ facet-toml/src/lib.rs
@@ -7,40 +7,50 @@ mod to_scalar;
 use core::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 use error::AnyErr;
-use facet_core::{Def, Facet};
+use facet_ansi::Stylize as _;
+use facet_core::{Def, Facet, StructKind};
 use facet_reflect::{ScalarType, Wip};
+use log::trace;
 use toml_edit::{DocumentMut, Item, TomlError};
 
 /// Deserializes a TOML string into a value of type `T` that implements `Facet`.
 pub fn from_str<T: Facet>(toml: &str) -> Result<T, AnyErr> {
-    let wip = Wip::alloc::<T>();
-    let wip = from_str_value(wip, toml)?;
-    let heap_value = wip.build().map_err(|e| AnyErr(e.to_string()))?;
-    heap_value
-        .materialize::<T>()
-        .map_err(|e| AnyErr(e.to_string()))
-}
+    trace!("Starting deserialization");
 
-fn from_str_value<'a>(wip: Wip<'a>, toml: &str) -> Result<Wip<'a>, AnyErr> {
+    let wip = Wip::alloc::<T>();
+
     let docs: DocumentMut = toml.parse().map_err(|e| TomlError::to_string(&e))?;
-    deserialize_item(wip, docs.as_item())
+    let wip = deserialize_item(wip, docs.as_item())?;
+
+    let heap_value = wip.build().map_err(|e| AnyErr(e.to_string()))?;
+    let result = heap_value
+        .materialize::<T>()
+        .map_err(|e| AnyErr(e.to_string()))?;
+
+    trace!("Finished deserialization");
+
+    Ok(result)
 }
 
 fn deserialize_item<'a>(wip: Wip<'a>, item: &Item) -> Result<Wip<'a>, AnyErr> {
-    let shape = wip.shape();
-    match shape.def {
+    trace!("Deserializing {}", item.type_name().blue());
+
+    match wip.shape().def {
         Def::Scalar(_) => deserialize_as_scalar(wip, item),
         Def::List(_) => deserialize_as_list(wip, item),
         Def::Map(_) => deserialize_as_map(wip, item),
         Def::Struct(_) => deserialize_as_struct(wip, item),
         Def::Enum(_) => deserialize_as_enum(wip, item),
+        Def::Option(_) => deserialize_as_option(wip, item),
         Def::SmartPointer(_) => deserialize_as_smartpointer(wip, item),
-        _ => Err(AnyErr(format!("Unsupported type: {:?}", shape))),
+        _ => Err(AnyErr(format!("Unsupported type: {:?}", wip.shape()))),
     }
 }
 
 fn deserialize_as_struct<'a>(mut wip: Wip<'a>, item: &Item) -> Result<Wip<'a>, AnyErr> {
-    // Parse as the inner struct type if item is a single value and the struct is a unit struct
+    trace!("Deserializing {}", "struct".blue());
+
+    // Parse as a the inner struct type if item is a single value and the struct is a unit struct
     if item.is_value() {
         // Only allow unit structs
         let shape = wip.shape();
@@ -81,41 +91,171 @@ fn deserialize_as_struct<'a>(mut wip: Wip<'a>, item: &Item) -> Result<Wip<'a>, A
         wip = wip.pop().map_err(|e| AnyErr(e.to_string()))?;
     }
 
+    trace!("Finished deserializing {}", "struct".blue());
+
     Ok(wip)
 }
 
 fn deserialize_as_enum<'a>(wip: Wip<'a>, item: &Item) -> Result<Wip<'a>, AnyErr> {
-    if item.is_value() {
-        let variant_name = item
-            .as_str()
-            .ok_or_else(|| AnyErr(format!("Expected string, got: {}", item.type_name())))?;
+    trace!("Deserializing {}", "enum".blue());
 
-        // Use variant_named to select the variant by name
-        wip.variant_named(variant_name).map_err(|e| {
-            AnyErr(format!(
-                "Error selecting enum variant '{}': {}",
-                variant_name, e
-            ))
-        })
-    } else {
-        // TODO: Handle non-unit enum variants
-        Err(AnyErr("Non-unit enum variants not yet supported".into()))
+    let wip = match item {
+        Item::None => todo!(),
+
+        Item::Value(value) => {
+            trace!("Entering {}", "value".cyan());
+
+            // A value can be an inline table, so parse it as such
+            if let Some(inline_table) = value.as_inline_table() {
+                if let Some((key, field)) = inline_table.iter().next() {
+                    trace!(
+                        "Entering {} with key {}",
+                        "inline table".cyan(),
+                        key.cyan().bold()
+                    );
+
+                    if inline_table.len() > 1 {
+                        return Err(AnyErr(
+                            "Cannot parse enum from inline table because it got multiple fields"
+                                .to_string(),
+                        ));
+                    } else {
+                        return build_enum_from_variant_name(
+                            wip,
+                            key,
+                            // TODO: remove clone
+                            &Item::Value(field.clone()),
+                        );
+                    }
+                } else {
+                    return Err(AnyErr(
+                        "Inline table doesn't have any fields to parse into enum variant"
+                            .to_string(),
+                    ));
+                }
+            }
+
+            let variant_name = value
+                .as_str()
+                .ok_or_else(|| format!("Expected string, got: {}", value.type_name()))?;
+
+            build_enum_from_variant_name(wip, variant_name, item)?
+        }
+
+        Item::Table(table) => {
+            if let Some((key, field)) = table.iter().next() {
+                trace!("Entering {} with key {}", "table".cyan(), key.cyan().bold());
+
+                if table.len() > 1 {
+                    return Err(AnyErr(
+                        "Cannot parse enum from inline table because it got multiple fields"
+                            .to_string(),
+                    ));
+                } else {
+                    build_enum_from_variant_name(wip, key, field)?
+                }
+            } else {
+                return Err(AnyErr(
+                    "Inline table doesn't have any fields to parse into enum variant".to_string(),
+                ));
+            }
+        }
+
+        Item::ArrayOfTables(_array_of_tables) => todo!(),
+    };
+
+    trace!("Finished deserializing {}", "enum".blue());
+
+    Ok(wip)
+}
+
+fn build_enum_from_variant_name<'a>(
+    mut wip: Wip<'a>,
+    variant_name: &str,
+    item: &Item,
+) -> Result<Wip<'a>, AnyErr> {
+    // Select the variant
+    wip = wip
+        .variant_named(variant_name)
+        .map_err(|e| AnyErr(e.to_string()))?;
+    // Safe to unwrap because the variant got just selected
+    let variant = wip.selected_variant().unwrap();
+
+    if variant.data.kind == StructKind::Unit {
+        // No need to do anything, we can just set the variant since it's a unit enum
+        return Ok(wip);
     }
+
+    // Push all fields
+    for (index, field) in variant.data.fields.iter().enumerate() {
+        wip = wip
+            .field_named(field.name)
+            .map_err(|e| format!("Field by name on enum does not exist: {e}"))?;
+
+        // Try to get the TOML value as a table to extract the field
+        if let Some(item) = item.as_table_like() {
+            // Base the field name on what type of struct we are
+            let field_name = if let StructKind::TupleStruct | StructKind::Tuple = variant.data.kind
+            {
+                &index.to_string()
+            } else {
+                // It must be a struct field
+                field.name
+            };
+
+            // Try to get the TOML field matching the Rust name
+            let Some(field) = item.get(field_name) else {
+                return Err(format!("TOML field '{}' not found", field_name).into());
+            };
+
+            wip = deserialize_item(wip, field)?;
+
+            wip = wip.pop().map_err(|e| AnyErr(e.to_string()))?;
+        } else if item.is_value() {
+            wip = deserialize_item(wip, item)?;
+        } else {
+            return Err(format!("TOML {} is not a recognized type", item.type_name()).into());
+        }
+    }
+
+    Ok(wip)
 }
 
-fn deserialize_as_list<'a>(mut wip: Wip<'a>, item: &Item) -> Result<Wip<'a>, AnyErr> {
-    todo!()
+fn deserialize_as_list<'a>(mut _wip: Wip<'a>, _item: &Item) -> Result<Wip<'a>, AnyErr> {
+    trace!("Deserializing {}", "array".blue());
+
+    trace!("Finished deserializing {}", "array".blue());
+
+    todo!();
 }
 
-fn deserialize_as_map<'a>(mut wip: Wip<'a>, item: &Item) -> Result<Wip<'a>, AnyErr> {
-    todo!()
+fn deserialize_as_map<'a>(mut _wip: Wip<'a>, _item: &Item) -> Result<Wip<'a>, AnyErr> {
+    trace!("Deserializing {}", "ap".blue());
+
+    trace!("Finished deserializing {}", "ap".blue());
+
+    todo!();
 }
 
-fn deserialize_as_smartpointer<'a>(mut wip: Wip<'a>, item: &Item) -> Result<Wip<'a>, AnyErr> {
-    todo!()
+fn deserialize_as_option<'a>(mut _wip: Wip<'a>, _item: &Item) -> Result<Wip<'a>, AnyErr> {
+    trace!("Deserializing {}", "ption".blue());
+
+    trace!("Finished deserializing {}", "ption".blue());
+
+    todo!();
+}
+
+fn deserialize_as_smartpointer<'a>(mut _wip: Wip<'a>, _item: &Item) -> Result<Wip<'a>, AnyErr> {
+    trace!("Deserializing {}", "smart".blue());
+
+    trace!("Finished deserializing {}", "smart".blue());
+
+    todo!();
 }
 
 fn deserialize_as_scalar<'a>(mut wip: Wip<'a>, item: &Item) -> Result<Wip<'a>, AnyErr> {
+    trace!("Deserializing {}", "scalar".blue());
+
     match ScalarType::try_from_shape(wip.shape())
         .ok_or_else(|| format!("Unsupported scalar type: {}", wip.shape()))?
     {
@@ -132,7 +272,9 @@ fn deserialize_as_scalar<'a>(mut wip: Wip<'a>, item: &Item) -> Result<Wip<'a>, A
         }
         #[cfg(feature = "std")]
         ScalarType::CowStr => {
-            todo!()
+            wip = wip
+                .put(std::borrow::Cow::Owned(to_scalar::string(item)?))
+                .map_err(|e| AnyErr(e.to_string()))?
         }
         ScalarType::F32 => {
             wip = wip
@@ -220,6 +362,8 @@ fn deserialize_as_scalar<'a>(mut wip: Wip<'a>, item: &Item) -> Result<Wip<'a>, A
         }
         _ => return Err(AnyErr(format!("Unsupported scalar type: {}", wip.shape()))),
     }
+
+    trace!("Finished deserializing {}", "scalar".blue());
 
     Ok(wip)
 }
