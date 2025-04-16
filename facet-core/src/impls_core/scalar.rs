3f7c6ea00c1e4454b4d90a10daea337d46e3c168
Amos Wenger <amos@bearcove.eu>
2025-04-16 11:49:45 +0200

    Add impls for Path & PathBuf (from std)

--- facet-core/src/impls_core/scalar.rs
+++ facet-core/src/impls_core/scalar.rs
@@ -543,3 +543,33 @@ unsafe impl Facet for core::net::Ipv6Addr {
             .build()
     };
 }
+
+unsafe impl Facet for std::path::PathBuf {
+    const SHAPE: &'static Shape = &const {
+        Shape::builder()
+            .id(ConstTypeId::of::<Self>())
+            .layout(Layout::new::<Self>())
+            .def(Def::Scalar(
+                ScalarDef::builder()
+                    .affinity(ScalarAffinity::path().build())
+                    .build(),
+            ))
+            .vtable(value_vtable!((), |f, _opts| write!(f, "PathBuf")))
+            .build()
+    };
+}
+
+unsafe impl Facet for &std::path::Path {
+    const SHAPE: &'static Shape = &const {
+        Shape::builder()
+            .id(ConstTypeId::of::<Self>())
+            .layout(Layout::new::<Self>())
+            .def(Def::Scalar(
+                ScalarDef::builder()
+                    .affinity(ScalarAffinity::path().build())
+                    .build(),
+            ))
+            .vtable(value_vtable!((), |f, _opts| write!(f, "Path")))
+            .build()
+    };
+}
